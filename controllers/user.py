2f4988ddba09af53b0c587709e788fa852b01215
heetae185 <heetae185@naver.com>
2022-09-20 01:04:41 +0900

    add login page

--- controllers/user.py
+++ controllers/user.py
@@ -1,4 +1,4 @@
-from flask import request, render_template, flash, redirect, url_for
+from flask import request, render_template, flash, redirect, url_for, session
 from .blueprint import user
 from .blueprint import product
 from models.user import User
@@ -27,4 +27,34 @@ def signup():
     
     User.insert_one(form_data)
     
+    return redirect(url_for('product.get_products'))
+
+
+#로그인 페이지 API
+@user.route('/signin')  #주소가 같아도 방식이 다르면 다른 API라고 인식함
+def signin_form():
+    return render_template('user_signin.html')
+
+
+#로그인 API
+@user.route('/signin', methods=['POST'])
+def signin():
+    form_data = request.form
+    #이메일, 패스워드 받아서 로그인 가능 여부 가늠
+    user = User.sign_in(form_data)
+    
+    if not user:
+        flash('이메일 주소 또는 비밀번호를 확인해주세요.')
+        return render_template('user_signin.html')
+    else:
+        #세션 이용
+        session['user_id'] = str(user['_id'])
+        return redirect(url_for('product.get_products'))
+    
+
+#로그아웃
+@user.route('/signout')
+def signout():
+    #세션 정보 없애면 로그아웃 됨
+    session.pop('user_id', None)
     return redirect(url_for('product.get_products'))
--- models/user.py
+++ models/user.py
@@ -1,14 +1,17 @@
 from datetime import datetime
 from .mongodb import conn_mongodb
 from bson import ObjectId
+from flask_bcrypt import generate_password_hash, check_password_hash    #각각 password 암호화, 암호화된 password 체크
 
 class User():
     @staticmethod
     def insert_one(form_data):
         db = conn_mongodb()
+        #비밀번호 해시 생성
+        password_hash = generate_password_hash(form_data['password'])
         db.users.insert_one({
             'email': form_data['email'],
-            'password': form_data['password'],
+            'password': password_hash,
             'created_at': int(datetime.now().timestamp()),
             'updated_at': int(datetime.now().timestamp())
     })
@@ -18,6 +21,17 @@ class User():
         user = db.users.find_one({'email': email})
         
         return False if user else True
+    
+    def sign_in(login_data):
+        db = conn_mongodb()
+        user = db.users.find_one({'email': login_data['email']})
+        
+        if not user:
+            return False
+        if not check_password_hash(user['password'], login_data['password']):
+            return False
+        
+        return user
             
         
         
